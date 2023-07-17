72f216cc60775853574a64601e2cf27989cd71d0
İhsan Eren Delibaş <hsan Eren Deliba@DESKTOP-RPEDS99>
2023-07-17 21:52:36 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ymyp_3_2_Metotlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
